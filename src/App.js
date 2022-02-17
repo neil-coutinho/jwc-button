3c4f6e443d68439ee848b48c5891e9f18748db8b
Neil Coutinho <33371261+neil-coutinho@users.noreply.github.com>
2022-02-17 08:16:27 +0530

    Global Styles Added global styles via createGlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 import ButtonTable from "./ButtonTable";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+body {
+
+  font-family: 'Roboto', sans-serif;
+}
+`;
 
 export default function App() {
-  return <ButtonTable />;
+  return (
+    <>
+      <GlobalStyles />
+      <ButtonTable />
+    </>
+  );
 }
--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { COLORS } from "./constants";
 
 const Button = ({ variant, size, children }) => {
-  return "TODO";
+  return <button>{children}</button>;
 };
 
 export default Button;
